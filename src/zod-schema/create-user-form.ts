000ded3f3f60715d750333b84606f7b2f7ed3efe
salah <salah@example.com>
2026-06-20 23:35:45 +0100

    fix some issues

--- src/zod-schema/create-user-form.ts
+++ src/zod-schema/create-user-form.ts
@@ -2,8 +2,8 @@ import { z } from "zod";
 
 export const createUserForm = z.object({
     username: z.string(),
-    firstname: z.string().optional(),
-    lastname: z.string().optional(),
+    firstname: z.string(),
+    lastname: z.string(),
     password: z.string().min(6, "Password must be at least 6 characters long"),
     phone_number: z.string(),
     verification_token: z.string().optional(),
